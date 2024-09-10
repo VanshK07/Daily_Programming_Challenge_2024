28773279b46a7df0cd72cba5affebfc56a9d5019
VanshK07 <109079876+VanshK07@users.noreply.github.com>
2024-09-10 20:55:31 +0530

    Create Day_2_missing_number.java

---
+++ Day_2_missing_number.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class MissingNumber {
+    public static int findMissingNumber(int[] arr, int n) {
+        int totalSum = n * (n + 1) / 2;
+        int arraySum = 0;
+        for (int num : arr) {
+            arraySum += num;
+        }
+        return totalSum - arraySum;
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 4, 5, 6};
+        int n = 6;  
+        int missingNumber = findMissingNumber(arr, n);
+        System.out.println("The missing number is: " + missingNumber);
+    }
+}
