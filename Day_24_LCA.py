4cb5c9bbed7f40ec0db27edc10717fbd8388f496
VanshK07 <109079876+VanshK07@users.noreply.github.com>
2024-10-03 00:33:33 +0530

    Create Day_24_LCA.py

---
+++ Day_24_LCA.py
@@ -0,0 +1,33 @@
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+def lowest_common_ancestor(root, p, q):
+    if not root or root == p or root == q:
+        return root
+    
+    left = lowest_common_ancestor(root.left, p, q)
+    right = lowest_common_ancestor(root.right, p, q)
+    
+    if left and right:
+        return root
+    
+    return left if left else right
+
+root = TreeNode(3)
+root.left = TreeNode(5)
+root.right = TreeNode(1)
+root.left.left = TreeNode(6)
+root.left.right = TreeNode(2)
+root.left.right.left = TreeNode(7)
+root.left.right.right = TreeNode(4)
+root.right.left = TreeNode(0)
+root.right.right = TreeNode(8)
+
+p = root.left         
+q = root.left.right.right  
+
+lca = lowest_common_ancestor(root, p, q)
+print(lca.val)  # Output: 5
